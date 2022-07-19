0d43ed401c6f1e5597a6e2e1cfd2aba0a710c61a
karansinghk1 <karansinghk1@outlook.com>
2022-07-19 21:16:08 +0530

    feat: add dummy data

--- lib/data/dummy.dart
+++ lib/data/dummy.dart
@@ -1,19 +1,33 @@
+import "dart:math";
 import "../models/category_model.dart";
 import "package:uuid/uuid.dart";
+import "package:flutter/material.dart";
 
 var uuid = const Uuid();
+var colors = [
+  Colors.redAccent,
+  Colors.greenAccent,
+  Colors.yellowAccent,
+  Colors.deepPurple,
+  Colors.blueAccent,
+  Colors.deepOrange
+];
+
+Color randomColors() {
+  return colors[Random().nextInt(4)];
+}
 
 var dummyCategories = [
-  Category(id: uuid.v1(), title: "title 1"),
-  Category(id: uuid.v1(), title: "title 2"),
-  Category(id: uuid.v1(), title: "title 3"),
-  Category(id: uuid.v1(), title: "title 4"),
-  Category(id: uuid.v1(), title: "title 5"),
-  Category(id: uuid.v1(), title: "title 6"),
-  Category(id: uuid.v1(), title: "title 7"),
-  Category(id: uuid.v1(), title: "title 8"),
-  Category(id: uuid.v1(), title: "title 9"),
-  Category(id: uuid.v1(), title: "title 10"),
-  Category(id: uuid.v1(), title: "title 11"),
-  Category(id: uuid.v1(), title: "title 12"),
+  Category(id: uuid.v1(), title: "title 1", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 2", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 3", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 4", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 5", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 6", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 7", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 8", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 9", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 10", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 11", color: randomColors()),
+  Category(id: uuid.v1(), title: "title 12", color: randomColors()),
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: "BodoniModa",
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: "BodoniModa",
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+          ),
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: const TextStyle(
+                color: Colors.red,
+              ),
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );
